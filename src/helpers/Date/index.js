a4671584c534c00eb10fd5a8373ebe97901fa91c
JuliaKardelice <julia.royer@live.fr>
2024-12-22 21:30:53 +0100

    incrementation pour les mois

--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -13,4 +13,5 @@ export const MONTHS = {
   12: "décembre",
 };
 
-export const getMonth = (date) => MONTHS[date.getMonth()];
+export const getMonth = (date) => MONTHS[date.getMonth()+1];
+/* Sinon il donne le mois précedent */
