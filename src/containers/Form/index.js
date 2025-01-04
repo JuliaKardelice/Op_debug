b18ba19c81eddc1ffa082018627b0cd27193b970
JuliaKardelice <julia.royer@live.fr>
2025-01-04 21:45:49 +0100

    ajout du onSuccess pour le message de validation

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,8 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess() // appeler la fonction onSuccess
+
       } catch (err) {
         setSending(false);
         onError(err);
